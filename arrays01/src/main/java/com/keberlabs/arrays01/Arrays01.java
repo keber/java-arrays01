2e9e0a6e39e71e1f1639c1504656cf74641689f2
Keber <keberflores@gmail.com>
2023-06-17 13:52:44 -0400

    Assignment: multiples of 5

--- arrays01/src/main/java/com/keberlabs/arrays01/Arrays01.java
+++ arrays01/src/main/java/com/keberlabs/arrays01/Arrays01.java
@@ -23,12 +23,15 @@ public class Arrays01 {
         
         // for (String myArray1 : myArray) {
         /*
+        // playground
         for(int i=0;i< myArray.length ; i++){
             System.out.println("posicion: " + i);
             System.out.println("Valor: " + myArray[i]);
         }
         */
         
+        /*
+        // playground
         System.out.println("Hola, bienvenido! " );
         for (int i = 0; i< myArray.length; i++ ){
             System.out.println("Dame el valor del número: " + (i+1) );
@@ -36,7 +39,24 @@ public class Arrays01 {
 
             System.out.println("El número en la posición " + i + " con el valor " + myArray[i] + " es " + parImpar(myArray[i]));
         }
+        */
         
+        /* Assignment: 
+            Ask user for a number (size), then create an array of given size and ask user for each element.
+            Validate if numbers are mutiple of 5, in such case show value's position and if it is multiple of 5.
+        */
+        System.out.println("Please enter the amount of numbers to evaluate: ");
+        Integer n = myScanner.nextInt();
+        Integer [] myArray3 = new Integer[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter number " + (i+1) + ":");
+            myArray3[i] = myScanner.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(myArray3[i]%5==0){
+                System.out.println("Value " + myArray3[i] + " in array position " + i + "(element number " + (i+1) + ")" + " is multiple of 5.");
+            }
+        }
         
     }
 }
