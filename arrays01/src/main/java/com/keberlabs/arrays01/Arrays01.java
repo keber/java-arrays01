9a9aa6149db01297f4161e5313c8394e42213bed
Keber <keberflores@gmail.com>
2023-06-17 13:12:52 -0400

    first commit

---
+++ arrays01/src/main/java/com/keberlabs/arrays01/Arrays01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.keberlabs.arrays01;
+
+/**
+ *
+ * @author Keber
+ */
+public class Arrays01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
