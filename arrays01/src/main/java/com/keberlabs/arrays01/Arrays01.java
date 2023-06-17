c1c72c4f56bde87d07ed001bc9f3e4ef6dcfc36f
Keber <keberflores@gmail.com>
2023-06-17 13:23:58 -0400

    sandplay with arrays

--- arrays01/src/main/java/com/keberlabs/arrays01/Arrays01.java
+++ arrays01/src/main/java/com/keberlabs/arrays01/Arrays01.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.keberlabs.arrays01;
+
+import java.util.*;
 
 /**
  *
@@ -11,6 +12,23 @@ package com.keberlabs.arrays01;
 public class Arrays01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String [] myArray = new String[5];
+        myArray[0] = "Omar";
+        myArray[1] = "Hanz";
+        myArray[2] = "Diego";
+        myArray[3] = "Sofia";
+        myArray[4] = "Daniela";
+        
+        // for (String myArray1 : myArray) {
+        for(int i=0;i< myArray.length ; i++){
+            System.out.println("posicion: " + i);
+            System.out.println("Valor: " + myArray[i]);
+        }
+        
+        Integer [] myArray2 = { 5,6,7,8 };
+        for (int i = 0; i< myArray2.length; i++ ){
+            System.out.println("posicion: " + i);
+            System.out.println("Valor: " + myArray2[i]);
+        }
     }
 }
